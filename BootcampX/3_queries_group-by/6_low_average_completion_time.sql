f8664ac449ae486276f9be594235ee59f4804655
Lee <notayogilee@gmail.com>
2020-03-03 01:37:01 +0000

    time to go home

---
+++ BootcampX/3_queries_group-by/6_low_average_completion_time.sql
@@ -0,0 +1,6 @@
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
+FROM students
+JOIN assignment_submissions ON students.id = student_id
+JOIN assignments ON assignment_id = assignments.id
+WHERE students.end_date IS NULL
+ORDER BY average_estimated_duration;
